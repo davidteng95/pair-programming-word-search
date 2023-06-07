7816b3818185f6b79109a00e11b2304cc1a5439e
David <davidteng95@gmail.com>
2023-06-07 23:06:39 +0000

    Pair Programming - Word Search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
+const transpose = require("./transposeFunction")
 const wordSearch = (letters, word) => { 
+  if (letters.length !== 0) {
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+    // console.log(horizontalJoin);
+    const verticalJoin = transpose(horizontalJoin).map(ls => ls.join(""));
+    // console.log(verticalJoin);
 
-module.exports = wordSearch
+    for (l of horizontalJoin) {
+      if (l.includes(word)) return true
+    }
+
+    for (l of verticalJoin) {
+      if (l.includes(word)) return true
+    }
+    return false;
+  }
+};
+
+module.exports = wordSearch;
